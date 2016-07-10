b694fb0e78a2c5af1d96a4c5b8d098f4074e44d7
Pierre V <pierre.varlez@gmail.com>
2016-07-10 02:02:59 +0200

    Fixing config file parsing

--- main.c
+++ main.c
@@ -5,32 +5,41 @@ char *layout = "en\0";
 char *browser = "firefox\0";
 bool visible = true;
 
-void printHelp() {
+void print_help() {
 	printf("Yet to be done\n");
 }
 
-void getConfig() {
+void parse_config() {
 	FILE *configFile = fopen("config.conf", "r");
 	
 	char *line = NULL;
 	size_t len = 100;
 	ssize_t read;
 
+	/* Reading file line by line */
 	while((read = getline(&line, &len, configFile)) != -1) {
+		/* Removing commented and empty lines */
 		if(line[0] != '#' && line[0] != '\n') {
+			/* Splitting lines at '=' character */
 			char *var = strtok(line, "=");
 			
+
 			if(strcmp(var,"lang") == 0) {
+				/* Duplicating string and assigning it to variable*/
 				lang = strdup(strtok(NULL, "="));
+				/* Adding \0 char to make a well-made CString */
+				lang[strlen(lang)-1] = '\0';
 			} else if(strcmp(var,"layout") == 0) {
 				layout = strdup(strtok(NULL, "="));
+				layout[strlen(layout)-1]= '\0';
 			} else if(strcmp(var,"browser") == 0) {
 				browser = strdup(strtok(NULL, "="));
+				browser[strlen(browser)-1]= '\0';
 			} else if(strcmp(var,"visible") == 0) {
 	//			if(strcmp(strtok(NULL, "="),"true") == 0)
 	//				visible = true;
 	//			else visible = false;
-			} else {	
+			} else {
 				printf("Mistake on this line : %s\n", line);
 			}
 		}
@@ -40,7 +49,7 @@ void getConfig() {
 }
 
 int main(int argc, char **argv)  {
-	getConfig();
+	parse_config();
 
 	printf("lang : %s\nlayout : %s\nbrowser : %s", lang, layout, browser);
 	int c;
@@ -49,7 +58,7 @@ int main(int argc, char **argv)  {
 		int this_option_optind = optind ? optind : 1;
 		int option_index = 0;
 		/* Listing options */
-		static struct option longOptions[] = {
+		static struct option long_options[] = {
 			/* name, has_arg, flag, val */
 			{"help", no_argument, 0, 'h'},
 			{"config", no_argument, 0, 0},
@@ -58,18 +67,18 @@ int main(int argc, char **argv)  {
 			{0,0,0,0}
 		};
 
-		c = getopt_long(argc, argv, "h", longOptions, &option_index);
+		c = getopt_long(argc, argv, "h", long_options, &option_index);
 		if(c == -1)
 			break;
 		switch (c) {
 			case 0:
-				if(strcmp(longOptions[option_index].name, "config") == 0)
+				if(strcmp(long_options[option_index].name, "config") == 0)
 					system("vim config.conf");
-				else if(strcmp(longOptions[option_index].name, "language") == 0)
+				else if(strcmp(long_options[option_index].name, "language") == 0)
 					lang = optarg;				
 				break;
 			case 'h':
-				printHelp();
+				print_help();
 				break;
 			case '?':
 				/* character not in the optstring. 
