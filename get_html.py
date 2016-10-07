386b1c98baf4ae1cebdb19cc98aaa5479257b1be
Etienne JODRY <etienne.jodry@hotmail.fr>
2016-10-07 08:12:28 +0000

    Nouvelle branche, travail sur l'adaptation de la spider

--- get_html.py
+++ get_html.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from scrapy.spider import BaseSpider
+import scrapy
 import lxml.etree
 import lxml.html
 
-class GetHtmlSpider(BaseSpider):
+class GetHtmlSpider(scrapy.Spider):
 	name = "getHtml"
-	start_urls = ["https://www.wikipedia.org"]
+	url = "https://www.wikipedia.org"
+	def start_requests(self):
+		yield scrapy.Request(url, self.parse)
 
 	def parse(self,response):
 		root = lxml.html.fromstring(response.body)
