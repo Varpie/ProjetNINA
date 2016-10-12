2df06933111a247ab016e7a5ad12aaaa2c150de2
Neah-Ko <etienne.jodry@hotmail.fr>
2016-10-12 17:13:52 +0200

    séparation des taches d'ouverture du navigateur et d'embbed du python

--- navigator.c
+++ navigator.c
@@ -21,41 +21,11 @@ int main()
 	} else {
 		child_pid = pid;
 		//Create command to kill process and execute it with bash
-		char command[20] = {0};
-		sprintf(command,"kill -9 %d",child_pid);
+		//char command[20] = {0};
+		//sprintf(command,"kill -9 %d",child_pid);
 
-		sleep(10);
-		system(command);
-
-
-		// PyObject est un wrapper Python autour des objets qu'on
-		// va échanger enter le C et Python.
-		PyObject *retour, *module, *fonction, *arguments;
-		char *resultat;
-
-		// Initialisation de l'interpréteur(1 à la fois)
-		Py_Initialize();
-
-		// Import du script.
-		PySys_SetPath("."); // Le dossier en cours n'est pas dans le PYTHON PATH
-		module = PyImport_ImportModule("get_html");
-		// Récupération de la fonction
-		fonction = PyObject_GetAttrString(module, "make_requests_from_url");
-
-		// Création d'un PyObject de type string.
-		// https://docs.python.org/2/c-api/arg.html#c.Py_BuildValue
-		arguments = Py_BuildValue("(s)", "www.google.com");
-
-		// Appel de la fonction.
-		retour = PyEval_CallObject(fonction, arguments);
-
-
-		// Conversion du PyObject obtenu en string C
-		PyArg_Parse(retour, "s", &resultat);
-		printf("Resultat: %s\n", resultat);
-
-		//Important : on ferme l'interpréteur.
-		Py_Finalize();
+		//sleep(10);
+		//system(command);
 	}
 return 0;
 }
