0385667686809b4255e6f7024194ecde363833c9
Etienne <Etienne.JODRY@hotmail.fr>
2016-11-25 13:18:44 +0100

    ajout des fonctions de 'navigator' au main, merge des headers

--- main.h
+++ main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,3 +11,13 @@
 
 void print_help();
 void parse_config();
+
+typedef struct HyperLink{
+    char text[512];
+    char url[2048];
+} HyperLink;
+
+char * get_bodyhtml_from_url(char *url);
+int select_hyperlinks_from_html(char *html,struct HyperLink *links);
+
+#endif
