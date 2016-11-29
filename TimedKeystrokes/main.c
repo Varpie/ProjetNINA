486718a0d71f1684f9de3eb296d9ee22bafbd76e
Louis <louis.klein7@gmail.com>
2016-11-29 15:47:25 +0100

    Getting time of someone who types stuff is done

--- TimedKeystrokes/main.c
+++ TimedKeystrokes/main.c
@@ -1,3 +1,29 @@
-int main(int argc, cahr ** argv){
+#include <linux/input.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
+int main(int argc, char ** argv){
+ int fd = open("/dev/input/event0", O_RDONLY);
+ struct input_event ev;
+ char text[] = "this is a simple test to get your typing speed"; // there is no tricks just words that you have to type so we can use this information later thank you for your involvment in this typing test we really appreciate it";
+ int size = sizeof(text)/sizeof(char);
+ int t[size];
+ int i =0;
+ printf("Please write the following text : \n%s\n", text);
+ while(i<size-1){
+    read(fd, &ev, sizeof(struct input_event));
+    if(ev.type == 1 && ev.value == 1){
+        t[i] = (ev.time.tv_sec%1000)*1000000+ev.time.tv_usec;
+        i++;
+    }
+ }
+ int j = 0;
+ for(j=0;j<size-1;j++){
+   printf("\nDiff Temps %i : %i",j,t[j+1]-t[j]);
+ }
+ printf("\n");
+fflush(stdin);
 }
