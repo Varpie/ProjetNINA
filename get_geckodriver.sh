528cc7214101775eb89de04b3791e0ae1a984a2c
Pierre <pierre.varlez@gmail.com>
2016-12-09 16:42:25 +0100

    Downloading and placing geckodriver into /usr/bin/

---
+++ get_geckodriver.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz && tar -xvf geckodriver-v0.11.1-linux64.tar.gz && rm geckodriver-v0.11.1-linux64.tar.gz && mv geckodriver /usr/bin/
