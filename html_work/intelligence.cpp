1f9d5fe9ea42eb37902ac22d28c924ce300426de
Alix <alix.vuagnoux@gmail.com>
2016-12-15 10:24:55 +0100

    fixing my bullshit in Intelligence::roam

--- html_work/intelligence.cpp
+++ html_work/intelligence.cpp
@@ -27,7 +27,7 @@ void Intelligence::roam()
 			this->current_url = select_diff_random_in_vector(links,this->current_url).url;
 			//TODO : mettre l'url en question en blacklist
 		}
-		logging::vout("fin : ", this->current_url);
+		logging::vout("fin : " + this->current_url);
 	} while(x++ <= 5);
 }
 
@@ -55,17 +55,25 @@ HyperLink select_from_word_list(std::vector<HyperLink> &links,std::string url)
 	std::string line;
 	bool not_in_link = true;
 	int c = 0;
-	std::ifstream file("html_work/word_list.txt");
-	do {
-		link = select_random_in_vector(links);
+	int d;
+	std::ifstream file("word_list/word_list.txt");
+	if(file)
+		std::cout << "Fichier ouvert" << std::endl;
+	else
+		std::cout << "Fichier non ouvert" << std::endl;
+	link = select_random_in_vector(links);
+	/*do {
+		link = select_diff_random_in_vector(links, link.url);
 		std::cout << "Lien " << c << " : " << link.text << std::endl;
+		d = 0;
 		while(std::getline(file, line) && not_in_link) {
 			if(link.text.find(line) != std::string::npos) {
 				not_in_link = false;
 				std::cout << "Lien valide : " << link.text << std::endl;
 			}
+			std::cout << d << std::endl;
 		}
-	} while(not_in_link && c++<50);
+	} while(not_in_link && c++<50);*/
 	std::cout << "Lien retourné : " << link.url << std::endl;
 	return link;
 }
