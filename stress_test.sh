f8e232a7122fd5db01e13d806ab7407b20c9db52
Pierre <pierre.varlez@gmail.com>
2017-01-03 17:37:44 +0100

    Test stops if failing to find process

--- stress_test.sh
+++ stress_test.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-current_date=$(date +%s)
+starting_date=$(date +%s)
 mkdir -p log
 make && ./main --verbose 1>>log/log.txt 2>>log/error.txt &
+timeout=0
 while sleep 1; do
-    echo $(( $(date +%s) - $current_date ))
-    ps --no-headers -o pmem,pcpu,cputime,cp $(ps -e | grep firefox | awk 'BEGIN{if(!$1){print "nope"}else{print $1}}') # à finir
+    echo $(( $(date +%s) - $starting_date ))
+    gff=$(ps -e | grep firefox)
+    if [[ ${#gff} -ne 0 ]]; then
+        ps --no-headers -o pmem,pcpu,cputime,cp $(echo $gff | awk '{print $1}')
+    elif [[ $timeout -gt 3 ]]; then
+        exit
+    else
+        (( timeout++ ))
+    fi
 done
