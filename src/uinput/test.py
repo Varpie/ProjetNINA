74b2795f6604a1d2390982cf0e4540bdea1037f7
Etienne <Etienne.JODRY@hotmail.fr>
2017-01-17 16:33:22 +0100

    rm test.py

--- src/uinput/test.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-import uinput_wrapping_module
-dir(uinput_wrapping_module)
-stri = "Hello World !!"
-# print uinput_wrapping_module.send_a_button_default_func(65)
-print uinput_wrapping_module.write_array_func("Hello World !!")
