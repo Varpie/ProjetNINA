dddc8334e685750849dc41556b0adb2bcf6f9a62
Etienne <Etienne.JODRY@hotmail.fr>
2017-01-26 11:47:16 +0100

    Error catching

--- src/navigation/navigator.cpp
+++ src/navigation/navigator.cpp
@@ -5,8 +5,6 @@ Navigator::Navigator(void)
   logging::vout("Creating Navigator object");
   Py_Initialize();
   logging::vout("Python initialized");
-  /* we'll remove this action later and place main.py directly in the path */
-  /* PATH : /usr/local/lib/python2.7/dist-packages */
   if(PyRun_SimpleString("import sys;sys.path.insert(0, './src/navigation/')")) {
     logging::verr("path expansion failed");
   }
@@ -28,6 +26,7 @@ Navigator::~Navigator(void)
 
 void Navigator::call_python_function_void_nargs(std::string function)
 {
+  logging::vout("Enter : Call python function void nargs");
   PyObject *fonction;
   fonction = PyObject_GetAttrString(module, function.c_str());
   if(fonction == NULL) {
@@ -36,10 +35,12 @@ void Navigator::call_python_function_void_nargs(std::string function)
   }
   PyEval_CallObject(fonction,NULL);
   Py_DECREF(fonction);
+  logging::vout("Out : Call python function void nargs");
 }
 
 std::string Navigator::call_python_function_nargs(std::string function)
 {
+  logging::vout("Enter : Call python function nargs");
   char *resultat;
   PyObject *fonction, *retour;
   fonction = PyObject_GetAttrString(module, function.c_str());
@@ -57,12 +58,14 @@ std::string Navigator::call_python_function_nargs(std::string function)
 
   std::string cpp_str = resultat;
   Py_DECREF(retour);
+  logging::vout("Out : Call python function nargs");
   return cpp_str;
 }
 
 
 std::string Navigator::call_python_function(std::string function,std::string arg)
 {
+  logging::vout("Enter : Call python function");
   char *resultat;
   PyObject *retour, *fonction, *arguments;
 
@@ -94,6 +97,7 @@ std::string Navigator::call_python_function(std::string function,std::string arg
 
   std::string cpp_str = resultat;
   Py_DECREF(retour);
+  logging::vout("Out : Call python function");
   return cpp_str;
 }
 
@@ -125,56 +129,24 @@ void Navigator::select_hyperlinks_from_html(std::string html, std::vector<HyperL
     HyperLink lk;
     size_t b_tag_a = html.find("<a ");
     size_t e_tag_a = html.find("</a>");
-    std::string tag_a = html.substr(b_tag_a, e_tag_a - b_tag_a);
-    if((e_tag_a+4) <= html.size())
-      html.erase(0,e_tag_a+4);
-    else
-      break;
-    size_t b_href = tag_a.find("href=\"");
-    size_t e_href = tag_a.substr(b_href+6).find("\"");
-    size_t b_txt_a = tag_a.find(">");
-    lk.url = tag_a.substr(b_href+6,e_href);
-    lk.text = tag_a.substr(b_txt_a+1);
-    while(lk.text.find("</") != std::string::npos) {
-      size_t b_close = lk.text.find("</");
-      size_t e_close = lk.text.find(">",b_close);
-      std::string type_tag = lk.text.substr(b_close+2,e_close - 2 - b_close);
-      size_t b_open = lk.text.find("<"+type_tag);
-      size_t e_open = lk.text.find(">",b_open);
-      if((e_close - b_close +1) <= lk.text.size())
-        lk.text = lk.text.erase(b_close,e_close - b_close +1);
-      else
+    if(b_tag_a == std::string::npos || e_tag_a == std::string::npos ||
+      ((html.size()-b_tag_a) < (e_tag_a - b_tag_a)) || (html.size() < (e_tag_a+4))) {
         break;
-      if((e_open - b_open +1) <= lk.text.size())
-        lk.text = lk.text.erase(b_open,e_open - b_open +1);
-      else
-        break;
-      std::string::size_type i = 0;
-      /* remove \n */
-      while(i < lk.text.length()) {
-        i = lk.text.find('\n',i);
-        if(i == std::string::npos) {
-            break;
-        }
-        lk.text.erase(i);
-      }
     }
+    std::string tag_a = html.substr(b_tag_a, e_tag_a - b_tag_a);
+    html.erase(0,e_tag_a+4);
+    int a = parse_tag_a(lk,tag_a);
+    if(a == 1)
+      break;
+    else if(a == 2)
+      continue;
     /* remove links like the ones in rubbish list */
     if(!rubbish.empty()){
-      bool insert = true;
       for(auto const& rub: rubbish) {
         if(lk.url.find(rub) != std::string::npos){
-          insert = false;
+          // std::cout << lk.url << std::endl;
+          continue;
         }
-      }
-      if(!insert)
-        continue;
-    }
-    /* remove urls with extensions other than .php or .html such as .pdf, .png and so on... */
-    if(lk.url.length()>5){
-      std::string ext = lk.url.substr(lk.url.length()-6);
-      if((ext.find('.') != std::string::npos && (lk.url.substr(lk.url.length()-4) != ".php" || lk.url.substr(lk.url.length()-5) != ".html"))){
-        continue;
       }
     }
     links.push_back(lk);
@@ -189,11 +161,22 @@ void Navigator::select_hyperlinks_from_html(std::string html, std::vector<HyperL
     HyperLink lk;
     size_t b_tag_a = html.find("<a ");
     size_t e_tag_a = html.find("</a>");
-    std::string tag_a = html.substr(b_tag_a, e_tag_a - b_tag_a);
-    if((e_tag_a+4) <= html.size())
-      html.erase(0,e_tag_a+4);
-    else
+    if(b_tag_a == std::string::npos || e_tag_a == std::string::npos ||
+      ((html.size()-b_tag_a) < (e_tag_a - b_tag_a)) || (html.size() < (e_tag_a+4))) {
       break;
+    }
+    std::string tag_a = html.substr(b_tag_a, e_tag_a - b_tag_a);
+    html.erase(0,e_tag_a+4);
+    int a = parse_tag_a(lk,tag_a);
+    if(a == 1)
+      continue;
+    links.push_back(lk);
+  }
+}
+
+int Navigator::parse_tag_a(HyperLink &lk,std::string &tag_a) {
+  /* nothing = 0 continue = 1 */
+  try {
     size_t b_href = tag_a.find("href=\"");
     size_t e_href = tag_a.substr(b_href+6).find("\"");
     size_t b_txt_a = tag_a.find(">");
@@ -205,20 +188,14 @@ void Navigator::select_hyperlinks_from_html(std::string html, std::vector<HyperL
       std::string type_tag = lk.text.substr(b_close+2,e_close - 2 - b_close);
       size_t b_open = lk.text.find("<"+type_tag);
       size_t e_open = lk.text.find(">",b_open);
-      if((e_close - b_close +1) <= lk.text.size())
-        lk.text = lk.text.erase(b_close,e_close - b_close +1);
-      else
-        break;
-      if((e_open - b_open +1) <= lk.text.size())
-        lk.text = lk.text.erase(b_open,e_open - b_open +1);
-      else
-        break;
+      lk.text = lk.text.erase(b_close,e_close - b_close +1);
+      lk.text = lk.text.erase(b_open,e_open - b_open +1);
       std::string::size_type i = 0;
       /* remove \n */
       while(i < lk.text.length()) {
         i = lk.text.find('\n',i);
         if(i == std::string::npos) {
-            break;
+          break;
         }
         lk.text.erase(i);
       }
@@ -227,9 +204,12 @@ void Navigator::select_hyperlinks_from_html(std::string html, std::vector<HyperL
     if(lk.url.length()>5){
       std::string ext = lk.url.substr(lk.url.length()-6);
       if((ext.find('.') != std::string::npos && (lk.url.substr(lk.url.length()-4) != ".php" || lk.url.substr(lk.url.length()-5) != ".html"))){
-        continue;
+        return 1;
       }
     }
-    links.push_back(lk);
+    return 0;
+  } catch (const std::out_of_range &e) {
+    logging::vout("Error : " + (std::string)e.what());
+    return 1;
   }
 }
