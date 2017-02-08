73ca5ce3c8ab08cd253736585c0281de2226ca3e
Etienne <Etienne.JODRY@hotmail.fr>
2017-02-08 20:50:59 +0100

    Compilation fix

--- src/ctrl_keyboard/setup.py
+++ src/ctrl_keyboard/setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
 # define the extension module
-uinput_wrapping_module = Extension('uinput_wrapping_module', sources=['uinput_wrapping_module.c'])
+uinput_wrapping_module = Extension('uinput_wrapping_module', sources=['uinput_wrapping_module.c','write_keyboard.c','timed_keystrokes.c'])
 # run the setup
 setup(ext_modules=[uinput_wrapping_module],
     name='Uinput wrapping module',
