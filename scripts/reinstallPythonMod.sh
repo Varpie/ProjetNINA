bc9d8b09cb18bbcfa4198347029ae489a1cb9efa
Etienne <Etienne.JODRY@hotmail.fr>
2017-02-13 17:49:11 +0100

    Reinstall script modified

--- scripts/reinstallPythonMod.sh
+++ scripts/reinstallPythonMod.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # Run in scripts/
-sudo rm /usr/local/lib/python2.7/dist-packages/uinput_wrapping_module.so && sudo rm /usr/local/lib/python2.7/dist-packages/Uinput_wrapping_module-1.0.egg-info && cd ../src/ctrl_keyboard && rm -r build/ && sudo python setup.py install
+sudo rm /usr/local/lib/python2.7/dist-packages/uinput_wrapping_module.so
+sudo rm /usr/local/lib/python2.7/dist-packages/Uinput_wrapping_module-1.0.egg-info
+cd ../src/ctrl_keyboard
+rm -r build/
+sudo python setup.py install
