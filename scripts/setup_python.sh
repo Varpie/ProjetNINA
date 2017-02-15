4ce591b988713b4abe7bd68a56c63f41f2576182
Etienne <Etienne.JODRY@hotmail.fr>
2017-02-15 18:00:34 +0100

    Setup scripts active at ./configure

---
+++ scripts/setup_python.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+printf "setup python modules..."
+
+pyMod=$(pip list --format columns | grep Uinput)
+pyMain=$(find /usr/local/lib/python2.7/dist-packages/ | grep nina.py)
+
+if [ -z "$pyMod" ]; then
+  cd src/ctrl_keyboard
+  sudo python setup.py install
+  cd -
+fi
+
+if [ -z "$pyMain" ]; then
+  sudo cp src/navigation/nina.py /usr/local/lib/python2.7/dist-packages/
+fi
+
+printf " done.\n"
