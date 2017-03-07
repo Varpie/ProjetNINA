fdd81188afac5b51979d074ce30459b39a0a49e6
Pierre <pierre.varlez@gmail.com>
2017-03-07 17:50:29 +0100

    Adapting stress test script

--- scripts/stress_test.sh
+++ scripts/stress_test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 starting_date=$(date +%s)
 mkdir -p log
-make && ./main --verbose 1>>log/log.txt 2>>log/error.txt &
+sudo make install && sudo nina --verbose 2 1>>log/log.txt 2>>log/error.txt &
 timeout=0
 # Init gnuplot files
 echo "" > ps.dat
 while sleep 1; do
     current_date=$(( $(date +%s) - $starting_date ))
-    gff=$(ps -e | grep firefox)
-    # Check if firefox process exists
+    gff=$(ps -e | grep nina)
+    # Check if nina process exists
     if [[ ${#gff} -ne 0 ]]; then
         echo "$current_date $(ps --no-headers -o pmem,pcpu $(echo $gff | awk '{print $1}'))" >> ps.dat
     elif [[ $timeout -gt 3 ]]; then
