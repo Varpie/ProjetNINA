1b323c580f531352470a43f4fbb265609359bb43
Etienne <Etienne.JODRY@hotmail.fr>
2017-03-08 18:38:08 +0100

    Work on .deb

--- scripts/get_geckodriver.sh
+++ scripts/get_geckodriver.sh
@@ -2,8 +2,9 @@
 printf "checking for geckodriver..."
 
 gd=$(find /usr/local/bin/ -name geckodriver)
-if[ -z "$gd" ]; then
+
+if [ -z "$gd" ]; then
 	wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz && tar -xvf geckodriver-v0.11.1-linux64.tar.gz && rm geckodriver-v0.11.1-linux64.tar.gz && mv geckodriver /usr/local/bin/
 fi
 
-printf " done."
+printf " done.\n"
--- scripts/python_dependencies.sh
+++ scripts/python_dependencies.sh
@@ -3,7 +3,6 @@ printf "checking for python dependencies..."
 
 sel=$(pip list --format columns | grep selenium)
 lxml=$(pip list --format columns | grep lxml)
-gd=$(find /usr/local/bin/ -name geckodriver)
 flag=0
 message="\nThis program needs the following dependencie(s) :\n"
 
@@ -14,11 +13,6 @@ fi
 
 if [ -z "$sel" ]; then
   message+="\t-selenium\n"
-fi
-
-if [ -z "$gd" ]; then
-  message+="\t-geckodriver\n"
-  flag=1
 fi
 
 if [ "$flag" -ne "0" ]; then
@@ -41,9 +35,6 @@ if [ "$flag" -eq 1 ]; then
   fi
   if [ -z "$sel" ]; then
     sudo pip install selenium
-  fi
-  if [ -z "$gd" ]; then
-    wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz && tar -xvf geckodriver-v0.11.1-linux64.tar.gz && rm geckodriver-v0.11.1-linux64.tar.gz && mv geckodriver /usr/local/bin/
   fi
 fi
 
