76c42e06693da32b2dd6b43e686326b460102f39
Etienne <Etienne.JODRY@hotmail.fr>
2017-03-14 10:47:43 +0100

    Temp commit

--- scripts/get_geckodriver.sh
+++ scripts/get_geckodriver.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 printf "checking for geckodriver..."
 
-gd=$(find /usr/local/bin/ -name geckodriver)
+gd=$(ls -al | grep geckodriver)
 
 if [ -z "$gd" ]; then
-	wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz && tar -xvf geckodriver-v0.11.1-linux64.tar.gz && rm geckodriver-v0.11.1-linux64.tar.gz && mv geckodriver /usr/local/bin/
+	wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz && tar -xvf geckodriver-v0.11.1-linux64.tar.gz && rm geckodriver-v0.11.1-linux64.tar.gz
 fi
 
 printf " done.\n"
