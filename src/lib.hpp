933805314710298794b54a0b932eff8e720e6288
Alix <alix.vuagnoux@gmail.com>
2017-03-17 11:15:21 +0100

    Wtf git y u do dis

---
+++ src/lib.hpp
@@ -0,0 +1,42 @@
+#ifndef LOG_HPP_
+#define LOG_HPP_
+
+#include <atomic>
+
+namespace logging {
+    extern int verbose;
+    inline void vout(int level, std::string msg) {
+      if(verbose >= level)
+      std::cout << msg << std::endl;
+    }
+    inline void vout(std::string msg) {
+      vout(verbose,msg);
+    }
+    inline void verr(int level,std::string msg) {
+      if(verbose >= level)
+      std::cerr << msg << std::endl;
+    }
+    inline void verr(std::string msg) {
+      verr(1,msg);
+    }
+}
+
+namespace dict {
+    extern bool whitelist;
+    extern bool blacklist;
+    extern bool otherlist;
+    extern std::string whitefile;
+    extern std::string blackfile;
+    extern std::string otherfile;
+}
+
+namespace countdown {
+    extern bool links;
+    extern int number;
+}
+
+namespace threading {
+    extern std::atomic<bool> running;
+}
+
+#endif
